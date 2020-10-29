e780aad5f281bde0c493daa1df95fa183642c779
Marcelle <yanari@Marcelles-MacBook-Air.local>
2020-10-28 23:46:36 -0300

    structure, react router and styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+    </Switch>
   );
 }
 
